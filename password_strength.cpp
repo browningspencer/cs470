d8d0c3fe654fc32a2d684035269e79483da58e17
browningspencer <stbrowning4@gmail.com>
2019-07-06 16:00:56 -0600

    Update password_strength.cpp

--- password_strength.cpp
+++ password_strength.cpp
@@ -1,16 +1,14 @@
 /***********************************************************************
- * Program:
- *      Lab 11, Password Strength
- *      Brother Wilson, CS470
- * Author:
- *    Spencer Browning
- * Summary:
- *    Please include a detailed description of:
- *    1. What this program is designed to do
- *    2. How this program will go about solving the problem
- *    3. What assumptions about input (file or user) is made
- *    4. Citations if the algorithm was borrowed or inspired from any source
- ************************************************************************/
+* Program:
+*    Week 11, Password Strength
+*    Brother Wilson, CS470
+* Author:
+*    Spencer Browning
+* Summary:
+*    This program analyzes a password's strength.
+*    It will determine the possible cominations
+*    and how many bits the password is.
+************************************************************************/
 
 #include <iostream>
 #include <string>
