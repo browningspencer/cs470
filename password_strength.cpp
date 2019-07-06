fd21db2716f399ac720e8ab2b1e3a3658c906824
browningspencer <stbrowning4@gmail.com>
2019-07-06 16:09:35 -0600

    Update password_strength.cpp

--- password_strength.cpp
+++ password_strength.cpp
@@ -1,14 +1,14 @@
 /***********************************************************************
-* Program:
-*    Lab 11, Password Strength
-*    Brother Wilson, CS470
-* Author:
-*    Spencer Browning
-* Summary:
-*    This program analyzes a password's strength.
-*    It will determine the possible combinations
-*    and how many bits the password is.
-************************************************************************/
+ * Program:
+ *    Lab 11, Password Strength
+ *    Brother Wilson, CS470
+ * Author:
+ *    Spencer Browning
+ * Summary:
+ *    This program analyzes a password's strength.
+ *    It will determine the possible combinations
+ *    and how many bits the password is.
+ ************************************************************************/
 
 #include <iostream>
 #include <string>
@@ -27,15 +27,15 @@ void output(double combinations, int bits);
  *********************************************/
 int main()
 {
-    string input = userInput();
-    
-    double combinations = calculateCombinations(input);
-    
-    int bits = calculateBits(combinations);
-    
-    output(combinations, bits);
-    
-    return 0;
+   string input = userInput();
+   
+   double combinations = calculateCombinations(input);
+   
+   int bits = calculateBits(combinations);
+   
+   output(combinations, bits);
+   
+   return 0;
 }
 
 /*********************************************
@@ -43,12 +43,12 @@ int main()
  *********************************************/
 string userInput ()
 {
-    string password;
-    
-    cout << "Please enter the password: ";
-    cin >> password;
-    
-    return password;
+   string password;
+   
+   cout << "Please enter the password: ";
+   cin >> password;
+   
+   return password;
 }
 
 /*********************************************
@@ -58,14 +58,14 @@ string userInput ()
  *********************************************/
 double calculateCombinations(string password)
 {
-    int passwordLength = password.length();
-    
-    int alphabetSize = calculateAlphabetSize(password);
-    
-    double combinations = 0;
-    combinations = pow(alphabetSize, passwordLength);
-    
-    return combinations;
+   int passwordLength = password.length();
+   
+   int alphabetSize = calculateAlphabetSize(password);
+   
+   double combinations = 0;
+   combinations = pow(alphabetSize, passwordLength);
+   
+   return combinations;
 }
 
 /*********************************************
@@ -76,58 +76,58 @@ double calculateCombinations(string password)
  *********************************************/
 int calculateAlphabetSize(string password)
 {
-    int alphabetSize = 0;
-    
-    // Checks for the four types
-    bool foundNumber = false;
-    bool foundLowerCase = false;
-    bool foundUpperCase = false;
-    bool foundSymbol = false;
-    
-    char iterator = 'a';
-    
-    // iterating through each char in password string
-    for (int i = 0; i < password.length(); i++)
-    {
-        iterator = password[i];
-        if (ispunct(iterator))
-        {
-            if (foundSymbol)
-                continue;
-            
-            alphabetSize += 32;
-            foundSymbol = true;
-        }
-        
-        if (isdigit(iterator))
-        {
-            if (foundNumber)
-                continue;
-            
-            alphabetSize += 10;
-            foundNumber = true;
-        }
-        
-        if (isupper(iterator))
-        {
-            if (foundUpperCase)
-                continue;
-            
-            alphabetSize += 26;
-            foundUpperCase = true;
-        }
-        
-        if (islower(iterator))
-        {
-            if (foundLowerCase)
-                continue;
-            
-            alphabetSize += 26;
-            foundLowerCase = true;
-        }
-    }
-    
-    return alphabetSize;
+   int alphabetSize = 0;
+   
+   // Checks for the four types
+   bool foundNumber = false;
+   bool foundLowerCase = false;
+   bool foundUpperCase = false;
+   bool foundSymbol = false;
+   
+   char iterator = 'a';
+   
+   // iterating through each char in password string
+   for (int i = 0; i < password.length(); i++)
+   {
+      iterator = password[i];
+      if (ispunct(iterator))
+      {
+         if (foundSymbol)
+            continue;
+         
+         alphabetSize += 32;
+         foundSymbol = true;
+      }
+      
+      if (isdigit(iterator))
+      {
+         if (foundNumber)
+            continue;
+         
+         alphabetSize += 10;
+         foundNumber = true;
+      }
+      
+      if (isupper(iterator))
+      {
+         if (foundUpperCase)
+            continue;
+         
+         alphabetSize += 26;
+         foundUpperCase = true;
+      }
+      
+      if (islower(iterator))
+      {
+         if (foundLowerCase)
+            continue;
+         
+         alphabetSize += 26;
+         foundLowerCase = true;
+      }
+   }
+   
+   return alphabetSize;
 }
 
 /*********************************************
@@ -136,9 +136,9 @@ int calculateAlphabetSize(string password)
  *********************************************/
 int calculateBits(double combinations)
 {
-    int bits = (int)log2(combinations);
-    
-    return bits;
+   int bits = (int)log2(combinations);
+   
+   return bits;
 }
 
 /*********************************************
@@ -146,9 +146,9 @@ int calculateBits(double combinations)
  *********************************************/
 void output(double combinations, int bits)
 {
-    cout.setf(ios::fixed);
-    
-    cout << "There are " << setprecision(0) << combinations;
-    cout << " combinations" << endl;
-    cout << "That is equivalent to a key of " << bits << " bits" << endl;
+   cout.setf(ios::fixed);
+   
+   cout << "There are " << setprecision(0) << combinations;
+   cout << " combinations" << endl;
+   cout << "That is equivalent to a key of " << bits << " bits" << endl;
 }
